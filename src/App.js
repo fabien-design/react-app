a10a004122fe6c97096724cb5596f53d6ea964c2
fabien-design <fabienrozier60@gmail.com>
2024-02-07 13:34:08 +0100

    Exo 1 & 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import logo from './logo.svg';
+import videoThumbnail from "./video.svg";
 import './App.css';
 
-function App() {
+
+Object.prototype.propAccess = function(path) {
+  var array = path.split("."); // Split le path
+  var result = this; // Correspond a l'objet qui appelel 
+  array.forEach(element => {
+    if(result.hasOwnProperty(element)){ // verif si l'element du path existe dans 'l'object
+      result = result[element]; // si oui alors on avance dans l'objet
+    }else{
+      return undefined // autrement on return rien car ça n'existe pas
+    }
+  });
+  return result;
+}
+
+String.prototype.toto = function(Object) {
+  // var path = this.replace(/\{|\}/g,""); // on replace pour ne garder que le path
+  var path = this.replace(/^\{+|\}+$/g,""); // on replace pour ne garder que le path
+  return Object.propAccess(path);
+}
+
+function App({ video }) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World, Edit <code>src/App.js</code>
         </p>
         <a
           className="App-link"
@@ -18,8 +39,34 @@ function App() {
           Learn React
         </a>
       </header>
+     <main>
+      <Video video={video} />
+     </main>
     </div>
   );
 }
 
-export default App;
+function Thumbnail({video}) {
+  return (
+    <div className="thumbnail"> 
+      <a href={video.url}>
+      <img src={videoThumbnail} alt="video " />
+      </a>
+    </div>
+  )
+}
+
+function Video({ video }) {
+  return (
+    <div className="video">
+      <Thumbnail video={video} />
+      <a href={video.url}>
+        <h3>{video.title}</h3>
+        <p>{video.description}</p>
+      </a>
+      {/* <LikeButton video={video} /> */}
+    </div>
+  );
+}
+
+export { App, Video };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { App, Video } from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const video = {
+  title: "simpson Episode",
+  url: "https://www.youtube.com/watch?v=Ux_Mz6dPLpw",
+  description: "simpson episode 7 saison 7"
+}
 root.render(
   <React.StrictMode>
-    <App />
+    <App video={video} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+const car = {
+  brand: 'ffff',
+  modele: {
+    name: 'totot',
+    year: 2018
+  }
+}
+const year = car.propAccess('modele.year');
+const toto = "{{modele.year}}".toto(car);
+console.log(toto);
+
 reportWebVitals();
